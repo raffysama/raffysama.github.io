acb8a67a159fadbf80b677ee4c394b53f1923e0f
raffysama <raffysama@gmail.com>
2022-02-04 17:52:45 +0800

    added

--- js/script.js
+++ js/script.js
@@ -13,8 +13,16 @@ window.addEventListener('scroll', () =>{
 
 })
 
+document.querySelectorAll('.port-image img').forEach(image =>{
+  image.onclick = () =>{
+    document.querySelector('.popup-image').style.display = 'block';
+    document.querySelector('.popup-image img').src = image.getAttribute('src');
+  }
+});
 
-
+document.querySelector('.popup-image span').onclick = () =>{
+  document.querySelector('.popup-image').style.display = 'none';
+}
 
 
 
